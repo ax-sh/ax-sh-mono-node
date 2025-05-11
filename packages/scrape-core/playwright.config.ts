b54d3fa3701708d90433b21a0904eb7eeead6290
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-05-12 01:48:22 +0545

    style: standardize quotes in configuration files

--- packages/scrape-core/playwright.config.ts
+++ packages/scrape-core/playwright.config.ts
@@ -3,23 +3,24 @@ import { nxE2EPreset } from "@nx/playwright/preset";
 import { workspaceRoot } from "@nx/devkit";
 
 // For CI, you may want to set BASE_URL to the deployed application.
+// biome-ignore lint/complexity/useLiteralKeys: <explanation>
 const baseURL = process.env["BASE_URL"] || "http://localhost:3000";
 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// require('dotenv').config();
+import "dotenv/config";
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  ...nxE2EPreset(__filename, { testDir: "./e2e" }),
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  ...nxE2EPreset(import.meta.url, { testDir: "./e2e" }),
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions.    */
   use: {
     baseURL,
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer    */
     trace: "on-first-retry",
   },
   /* Run your local dev server before starting the tests */
@@ -34,12 +35,10 @@ export default defineConfig({
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
     },
-
     {
       name: "firefox",
       use: { ...devices["Desktop Firefox"] },
     },
-
     {
       name: "webkit",
       use: { ...devices["Desktop Safari"] },
